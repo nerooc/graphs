8f246fadf538a4a5b7ecfc0495c453829bb04145
BurningCodePieces <70864625+BurningCodePieces@users.noreply.github.com>
2021-04-25 00:47:35 +0200

    Update task2.py

--- Lab01/task2.py
+++ Lab01/task2.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     file_name=get_info_from_user()
     data_matrix=read_graph_file_return_Adjacency_matrix(file_name) #using the function from task 1
     graph=draw_nodes(data_matrix)
-    graph=draw_edges(graph,data_matrix)
+    graph=draw_edges_from_adjacency_matrix(graph,data_matrix)
     display_graph(graph)
