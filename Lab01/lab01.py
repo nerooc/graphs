f1c5001b3a08f782e78a214644d8bcda91e374ad
Bartosz Rogowski <65545041+bartosz-rogowski@users.noreply.github.com>
2021-04-28 22:39:34 +0200

    Added small fixes to function needed for task3
    Removed functions: `is_integer_num` and `generate_graph`
    Added functions: `generate_graph_a` and `generate_graph_b`

--- Lab01/lab01.py
+++ Lab01/lab01.py
@@ -7,182 +7,182 @@ import random #for task3
 # functions to task1 written by Karol Szeliga
 
 def read_graph_file_return_Adjacency_matrix(file_name, return_input_info=False):
-    """input:
-       file_name - file with correct representation of graph, for example:
-       Adjacency matrix:    Adjacency list:    Incidence matrix:
-       0 1 1                1  2 3             1 1
-       1 0 0                2  1               1 0
-       1 0 0                3  1               0 1
-       return_input_info - change output
+	"""input:
+	   file_name - file with correct representation of graph, for example:
+	   Adjacency matrix:    Adjacency list:    Incidence matrix:
+	   0 1 1                1  2 3             1 1
+	   1 0 0                2  1               1 0
+	   1 0 0                3  1               0 1
+	   return_input_info - change output
 
-      output (if return_input_info: a) else b) :
-       a) [Adjacency matrix]
-       b) [representation used in file, info which representation it is]
-         possible representations:
-         "adjacency matrix"
-         "adjacency list"
-         "incidence matrix" """
+	  output (if return_input_info: a) else b) :
+	   a) [Adjacency matrix]
+	   b) [representation used in file, info which representation it is]
+		 possible representations:
+		 "adjacency matrix"
+		 "adjacency list"
+		 "incidence matrix" """
 
-    try:
-        with open(file_name, "r") as f:
-            file_data = [[int(num) for num in line.split()] for line in f]
-            # decode which representation is in input
-            itIsList = False
-            for row in file_data:
-                for i in row:
-                    if i not in [0, 1]:
-                        itIsList = True
-                        f.seek(0)
-                        file_data2 = [[int(num) for num in remove_first(line.split())] for line in f]  # first int is number of vertex, must be removed
-                        if return_input_info:
-                            return file_data, "adjacency list"
-                        else:
-                            return convert_Adjacency_list_into_Adjacency_matrix(file_data2)
+	try:
+		with open(file_name, "r") as f:
+			file_data = [[int(num) for num in line.split()] for line in f]
+			# decode which representation is in input
+			itIsList = False
+			for row in file_data:
+				for i in row:
+					if i not in [0, 1]:
+						itIsList = True
+						f.seek(0)
+						file_data2 = [[int(num) for num in remove_first(line.split())] for line in f]  # first int is number of vertex, must be removed
+						if return_input_info:
+							return file_data, "adjacency list"
+						else:
+							return convert_Adjacency_list_into_Adjacency_matrix(file_data2)
 
-            if len(file_data) == len(file_data[0]):
-                isSymmetric = True  # only a suspicion so far
-            else:
-                if return_input_info:
-                    return file_data, "incidence matrix"
-                else:
-                    return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
+			if len(file_data) == len(file_data[0]):
+				isSymmetric = True  # only a suspicion so far
+			else:
+				if return_input_info:
+					return file_data, "incidence matrix"
+				else:
+					return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
 
-            for i in range(len(file_data) - 1):
-                for j in range(i + 1, len(file_data)):
-                    if file_data[i][j] != file_data[j][i]:
-                        isSymmetric = False
-                        if return_input_info:
-                            return file_data, "incidence matrix"
-                        else:
-                            return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
+			for i in range(len(file_data) - 1):
+				for j in range(i + 1, len(file_data)):
+					if file_data[i][j] != file_data[j][i]:
+						isSymmetric = False
+						if return_input_info:
+							return file_data, "incidence matrix"
+						else:
+							return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
 
-            diagonalOnlyZero = True
-            for i in range(len(file_data)):
-                if file_data[i][i] != 0:
-                    diagonalOnlyZero = False
-                    if return_input_info:
-                        return file_data, "incidence matrix"
-                    else:
-                        return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
+			diagonalOnlyZero = True
+			for i in range(len(file_data)):
+				if file_data[i][i] != 0:
+					diagonalOnlyZero = False
+					if return_input_info:
+						return file_data, "incidence matrix"
+					else:
+						return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
 
-            isTwoOneInCol = True
-            count = 0
-            for i in range(len(file_data)):
-                for j in range(len(file_data)):
-                    if file_data[j][i] == 1:
-                        count += 1
-                if count != 2:
-                    isTwoOneInCol = False
-                    if return_input_info:
-                        return file_data, "adjacency matrix"
-                    else:
-                        return file_data
-                else:
-                    count = 0
+			isTwoOneInCol = True
+			count = 0
+			for i in range(len(file_data)):
+				for j in range(len(file_data)):
+					if file_data[j][i] == 1:
+						count += 1
+				if count != 2:
+					isTwoOneInCol = False
+					if return_input_info:
+						return file_data, "adjacency matrix"
+					else:
+						return file_data
+				else:
+					count = 0
 
-            if all([not itIsList, isSymmetric, diagonalOnlyZero, isTwoOneInCol]):
-                print("Your matrix could be render as Adjacency matrix or Incidence matrix!\nType 'a' if you mean Adjacency matrix and 'b' if you mean Incidence matrix:")
-                while True:
-                    chosen_option = input()
-                    if chosen_option in ["a", "b"]:
-                        break
-                    else:
-                        print("Mistyped! Choose again.\nType 'a' if you mean Adjacency matrix and 'b' if you mean Incidence matrix:")
-                if chosen_option == "a":
-                    if return_input_info:
-                        return file_data, "adjacency matrix"
-                    else:
-                        return file_data
-                else:
-                    if return_input_info:
-                        return file_data, "incidence matrix"
-                    else:
-                        return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
+			if all([not itIsList, isSymmetric, diagonalOnlyZero, isTwoOneInCol]):
+				print("Your matrix could be render as Adjacency matrix or Incidence matrix!\nType 'a' if you mean Adjacency matrix and 'b' if you mean Incidence matrix:")
+				while True:
+					chosen_option = input()
+					if chosen_option in ["a", "b"]:
+						break
+					else:
+						print("Mistyped! Choose again.\nType 'a' if you mean Adjacency matrix and 'b' if you mean Incidence matrix:")
+				if chosen_option == "a":
+					if return_input_info:
+						return file_data, "adjacency matrix"
+					else:
+						return file_data
+				else:
+					if return_input_info:
+						return file_data, "incidence matrix"
+					else:
+						return convert_Incidence_matrix_into_Adjacency_matrix(file_data)
 
 
-    except FileNotFoundError:
-        print("Sorry, there is no file called '" + file_name + "'")
-        return []
+	except FileNotFoundError:
+		print("Sorry, there is no file called '" + file_name + "'")
+		return []
 
 
 def remove_first(_list):  # used in read_graph
-    _list.pop(0)
-    return _list
+	_list.pop(0)
+	return _list
 
 
 def convert_Adjacency_matrix_into_Adjacency_list(data_matrix):
-    for row in data_matrix:  # loop through a matrix if there is '1' somewhere, print with which vertex
-        adj_list = [[set_num(i, row[i]) for i in range(len(row))] for row in data_matrix]
-        for _list in adj_list:
-            for i in range(len(_list)):
-                if -1 in _list:
-                    _list.remove(-1)
-        return adj_list
+	for row in data_matrix:  # loop through a matrix if there is '1' somewhere, print with which vertex
+		adj_list = [[set_num(i, row[i]) for i in range(len(row))] for row in data_matrix]
+		for _list in adj_list:
+			for i in range(len(_list)):
+				if -1 in _list:
+					_list.remove(-1)
+		return adj_list
 
 
 def set_num(i, num):  # used in convert_Adjacency_matrix_into_Adjacency_list
-    if num != 0:
-        return i + 1
-    else:
-        return -1
+	if num != 0:
+		return i + 1
+	else:
+		return -1
 
 
 def convert_Adjacency_matrix_into_Incidence_matrix(data_matrix):
-    # count number of edges
-    count = 0
-    for i in range(len(data_matrix) - 1):
-        for j in range(i + 1, len(data_matrix)):
-            if data_matrix[i][j] != 0:
-                count += 1
+	# count number of edges
+	count = 0
+	for i in range(len(data_matrix) - 1):
+		for j in range(i + 1, len(data_matrix)):
+			if data_matrix[i][j] != 0:
+				count += 1
 
-    # make incidence matrix
-    incidence_matrix = [[0 for x in range(count)] for y in range(len(data_matrix))]
-    curr_edge = 0
-    for i in range(len(data_matrix) - 1):
-        for j in range(i + 1, len(data_matrix)):
-            if data_matrix[i][j] != 0:
-                incidence_matrix[i][curr_edge] = 1
-                incidence_matrix[j][curr_edge] = 1
-                curr_edge += 1
+	# make incidence matrix
+	incidence_matrix = [[0 for x in range(count)] for y in range(len(data_matrix))]
+	curr_edge = 0
+	for i in range(len(data_matrix) - 1):
+		for j in range(i + 1, len(data_matrix)):
+			if data_matrix[i][j] != 0:
+				incidence_matrix[i][curr_edge] = 1
+				incidence_matrix[j][curr_edge] = 1
+				curr_edge += 1
 
-    return incidence_matrix
+	return incidence_matrix
 
 
 def convert_Adjacency_list_into_Adjacency_matrix(data_matrix):
-    adjacency_matrix = [[0 for x in range(len(data_matrix))] for y in range(len(data_matrix))]
-    for i in range(len(data_matrix)):
-        for j in data_matrix[i]:
-            adjacency_matrix[i][j - 1] = 1
-    return adjacency_matrix
+	adjacency_matrix = [[0 for x in range(len(data_matrix))] for y in range(len(data_matrix))]
+	for i in range(len(data_matrix)):
+		for j in data_matrix[i]:
+			adjacency_matrix[i][j - 1] = 1
+	return adjacency_matrix
 
 
 def convert_Incidence_matrix_into_Adjacency_matrix(data_matrix):
-    adjacency_matrix = [[0 for x in range(len(data_matrix))] for y in range(len(data_matrix))]
+	adjacency_matrix = [[0 for x in range(len(data_matrix))] for y in range(len(data_matrix))]
 
-    w = -1
-    h = -1
-    for j in range(len(data_matrix[0])):
-        for i in range(len(data_matrix)):
-            if data_matrix[i][j] != 0:
-                if w == -1:
-                    w = i
-                else:
-                    h = i
-        adjacency_matrix[w][h] = 1
-        adjacency_matrix[h][w] = 1
-        w = -1
-        h = -1
-    return adjacency_matrix
+	w = -1
+	h = -1
+	for j in range(len(data_matrix[0])):
+		for i in range(len(data_matrix)):
+			if data_matrix[i][j] != 0:
+				if w == -1:
+					w = i
+				else:
+					h = i
+		adjacency_matrix[w][h] = 1
+		adjacency_matrix[h][w] = 1
+		w = -1
+		h = -1
+	return adjacency_matrix
 
 
 def convert_Incidence_matrix_into_Adjacency_list(data_matrix):
-    temp_adj_matrix = convert_Incidence_matrix_into_Adjacency_matrix(data_matrix)
-    return convert_Adjacency_matrix_into_Adjacency_list(temp_adj_matrix)
+	temp_adj_matrix = convert_Incidence_matrix_into_Adjacency_matrix(data_matrix)
+	return convert_Adjacency_matrix_into_Adjacency_list(temp_adj_matrix)
 
 
 def convert_Adjacency_list_into_Incidence_matrix(data_matrix):
-    temp_adj_matrix = convert_Adjacency_list_into_Adjacency_matrix(data_matrix)
-    return convert_Adjacency_matrix_into_Incidence_matrix(temp_adj_matrix)
+	temp_adj_matrix = convert_Adjacency_list_into_Adjacency_matrix(data_matrix)
+	return convert_Adjacency_matrix_into_Incidence_matrix(temp_adj_matrix)
 
 ###############################################################################################
 
@@ -289,54 +289,38 @@ def draw_graph(data_matrix,node_colors_tab=None):
 
 #task3 written by Bartosz Rogowski
 
-def is_integer_num(obj):
-	'''Function checking if the given argument is an integer
+
+def generate_graph_a(num_of_vertices: int, num_of_edges: int):
+	'''Function generating random graph (number of edges version)
 
 	Arguments:
-		obj {any} -- argument that will be checked
-		arg2 {integer or float} --  number of edges or probability that an edge exists between two vertices
+		num_of_vertices {integer} -- number of vertices
+		num_of_edges {integer} -- number of edges
 
 	Returns:
-		bool -- information whether given argument is an integer
-	'''
-	if isinstance(obj, int):
-		return True
-	elif isinstance(obj, float):
-		return obj.is_integer()
-	else:
-		print("Argument is neither integer nor float")
-		exit(-1)
-
-def generate_graph(n: int, arg2):
-	'''Function generating graph
-
-	Arguments:
-		n {integer} -- number of vertices
-		arg2 {integer or float} --  number of edges or probability that an edge exists between two vertices
-
-	Returns:
-		numpy array -- matrix representing graph
+		numpy array -- incidence matrix representing graph
 	'''
 
-	if(is_integer_num(arg2) and arg2 > 0):
-		l = int(arg2)
-		if(l > n*(n-1)/2):
-			print("Number of edges is too big")
+	if num_of_edges >= 0:
+		max_number_of_edges = int(num_of_vertices*(num_of_vertices-1)/2)
+		if(num_of_edges > max_number_of_edges):
+			print(f"Number of edges is greater than max value ({max_number_of_edges})")
 			exit(-1)
-		G = np.zeros((n, l))
+		G = np.zeros((num_of_vertices, num_of_edges))
 		pairing_set = set() #contains pairs of vertices between which edge already exists
-		for edge in range(0, l):
+		for edge in range(0, num_of_edges):
 			try:
 				#random.sample generates 2 unique values of given range
-				vertex = tuple(random.sample(range(0, n), 2))
-				#edge must be singular (simple graph) therefore we need to check whether it already exists or not 
+				vertex = tuple(random.sample(range(0, num_of_vertices), 2))
+				#edge must be singular (simple graph) therefore we need to check whether 
+				# it already exists or not 
 				if vertex not in pairing_set:
 					#adding pairs for example: (1, 4) and (4, 1) since both are the same edge
 					pairing_set.add(vertex)
 					pairing_set.add(vertex[::-1])
 				else:
 					while vertex in pairing_set:
-						vertex = tuple(random.sample(range(0, n), 2))
+						vertex = tuple(random.sample(range(0, num_of_vertices), 2))
 					pairing_set.add(vertex)
 					pairing_set.add(vertex[::-1])
 			except ValueError:
@@ -344,28 +328,41 @@ def generate_graph(n: int, arg2):
 				exit(-1)
 			for i in range(0, 2):
 				G[vertex[i], edge] += 1
+	else:
+		print("Second argument is not valid (must be positive integer")
+		exit(-1)
+	return G
 
-	elif(arg2 >= 0 and arg2 < 1):
-		p = arg2
-		G = np.zeros((n, n))
-		for a in range(0, n-1):
-			for b in range(0, n-1-a):
-				if random.random() <= p:
+def generate_graph_b(num_of_vertices: int, probability: float):
+	'''Function generating random graph (probability version)
+
+	Arguments:
+		num_of_vertices {integer} -- number of vertices
+		num_of_edges {integer} -- probability that an edge exists between two vertices
+
+	Returns:
+		numpy array -- adjacency matrix representing graph
+	'''
+	if(probability >= 0 and probability <= 1):
+		G = np.zeros((num_of_vertices, num_of_vertices))
+		for a in range(0, num_of_vertices-1):
+			for b in range(0, num_of_vertices-1-a):
+				if random.random() <= probability:
 					G[a, a+b+1] = 1
 				else:
 					G[a, a+b+1] = 0
-		for a in range(0, n):
-			for b in range(a, n):
+		for a in range(0, num_of_vertices):
+			for b in range(a, num_of_vertices):
 				G[b, a] = G[a, b]
 		
 		count = 0
-		for a in range(0, n):
-			for b in range(0, n):
+		for a in range(0, num_of_vertices):
+			for b in range(0, num_of_vertices):
 				if G[a, b] == 1:
 					count += 1
-		print(f"{100*count/(n**2)}%")
+		print(f"{100*count/(num_of_vertices**2 - num_of_vertices)}%")
 
 	else:
-		print("Second argument is not valid")
+		print("Second argument is not valid (must be float in range [0, 1]")
 		exit(-1)
 	return G
--- Lab01/task3.py
+++ Lab01/task3.py
@@ -8,17 +8,30 @@ Script by Bartosz Rogowski
 '''
 
 import numpy as np
-from lab01 import is_integer_num, generate_graph
+from lab01 import generate_graph_a, generate_graph_b
 
 if __name__ == '__main__':
-
-	number_of_vertices = input("Enter number of vertices: ")
-	second_arg = input("Enter number of edges or probability that an edge exists between two vertices: ")
-	try:
-		G = generate_graph(int(number_of_vertices), float(second_arg))
-	except ValueError:
-		print("Wrong arguments")
+	mode = input("Please type mode: \n- 'a' if second argument will be number of edges\n- 'b' - if probability\n")
+	if mode == 'a':
+		number_of_vertices = input("Enter number of vertices: ")
+		number_of_edges = input("Enter number of edges or probability that an edge exists between two vertices: ")
+		try:
+			G = generate_graph_a(int(number_of_vertices), int(number_of_edges))
+		except ValueError:
+			print("Wrong arguments")
+			exit(-1)
+	elif mode == 'b':
+		number_of_vertices = input("Enter number of vertices: ")
+		probability = input("Enter number of edges or probability that an edge exists between two vertices: ")
+		try:
+			G = generate_graph_b(int(number_of_vertices), float(probability))
+		except ValueError:
+			print("Wrong arguments")
+			exit(-1)
+	else:
+		print("Incorrect mode, please try again.")
 		exit(-1)
+	
 	#save G to file
 	np.savetxt("InputFiles/task3.txt", G, delimiter=" ", newline = "\n", fmt="%d")
-	print("Output file has been saved to: 'task3.txt'")
+	print("Output file has been saved to: 'InputFiles/task3.txt'")
