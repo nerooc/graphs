892af42522707d488cdc8c09ead3af462555aa7f
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-05-08 17:56:51 +0200

    add using input args

--- Lab02/task2.py
+++ Lab02/task2.py
@@ -1,17 +1,21 @@
 import random
 import time
-# from pandas import *
+import sys
+from pandas import *
 from lab01 import read_graph_file_return_Adjacency_matrix
 from lab01 import generate_graph_b
 from lab01 import draw_graph
 from lab02 import *
 
+
+
+# main
 if __name__ == "__main__":
     # # some tests
     # prop = [0.01, 0.2, 0.4, 0.6, 0.8, 0.99]
     # ver = [20, 100, 300, 500]
     # arrTime = [[0 for i in range(len(prop))] for row in range(len(ver))]
-    # print("start!")
+    # print("Some tests...")
     # for i in range(len(prop)):
     #     for j in range(len(ver)):
     #         Adjacency_matrix = generate_graph_b(ver[j], prop[i])
@@ -21,11 +25,16 @@ if __name__ == "__main__":
     #         arrTime[j][i] = end - start
     #
     # print(DataFrame(arrTime))
+    # print()
 
-    print("This is graph randomization.\nType the name of your graph representation file:")
-    file_name = input()
+
+    print("This is graph randomization. Path to graph data file should be typed in input args")
+    file_name = 'InputFiles/input.txt'
+    if len(sys.argv) == 1:
+        print("No input in program args, trying to open file \'InputFiles/input.txt\'...\n")
+    else:
+        file_name = sys.argv[1]  # path to file with graph data
+
     Adjacency_matrix = read_graph_file_return_Adjacency_matrix(file_name)
-    print("Before randomization... (to see effect close first image)")
-    draw_graph(Adjacency_matrix)
-    print("After randomization")
-    draw_graph(graph_randomization(Adjacency_matrix))
+    draw_two_graphs(Adjacency_matrix, graph_randomization(Adjacency_matrix),
+                    'before randomization:', 'after randomization:')
