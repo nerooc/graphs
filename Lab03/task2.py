5e7e699205f5aae48e7ac9b483363ffb0eb77c3a
Tomasz Gajda <tomasz_gajda@outlook.com>
2021-05-08 22:27:32 +0200

    add some basic tests for lab03/task2

--- Lab03/task2.py
+++ Lab03/task2.py
@@ -10,7 +10,6 @@ from lab03 import dijkstra, print_dijkstra
 if __name__ == "__main__": 
     if len(sys.argv) != 2:
         raise Exception("Run as: task2.py [v_start]")
-    v_start = int(sys.argv[1])
 
     #graph will be generated after task 1 will be finished :)
 
@@ -23,6 +22,22 @@ if __name__ == "__main__":
             [0,  0,  0,  0,  0,  8,  0,  0,  0],
             [0,  4,  2,  0,  0,  0,  0,  0,  0],
             [4,  0,  0,  0,  8,  0,  0,  0,  0]]
+
+    #TESTING###########################################################################
+    #tests will be updated with task 1 finished
+    if(sys.argv[1] == "test"):
+        print("\nTEST #1 - Test for first vertex of the graph:\n")
+        path_costs, predecessors = dijkstra(graph, 1)
+        print_dijkstra(path_costs, predecessors, 1)
+        print("\nTEST #2 - Test for last vertex of the graph:\n")
+        path_costs, predecessors = dijkstra(graph, 1)
+        print_dijkstra(path_costs, predecessors, 1)
+        sys.exit("End of testing")
+    #TESTING###########################################################################
+    
+    v_start = int(sys.argv[1])
+
+    
 
     path_costs, predecessors = dijkstra(graph, v_start)
     print_dijkstra(path_costs, predecessors, v_start)
