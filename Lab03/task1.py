0efeb305869ec0cd83846804c09a966b5701b9ab
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-05-11 20:35:32 +0200

    final change

--- Lab03/task1.py
+++ Lab03/task1.py
@@ -1,16 +1,16 @@
 import sys
-from lab02 import draw_graph
-from lab03 import generate_random_graph
-from lab03 import add_weights
+from lab02 import draw_two_graphs
+from lab03 import generate_random_graph, add_int_weights
 
 if __name__ == "__main__":
-    print("This program generates random coherent graph. In input args set max vertices number.")
+    print("This program generates random complement graph. In input args set max vertices number.")
     max_vertices_num = 9
     if len(sys.argv) == 1:
         print("No input in program args, max_vertices_num = 9")
     else:
         max_vertices_num = sys.argv[1]  # path to file with graph data
+    # max_vertices_num = 4
     adjacency_matrix1 = generate_random_graph(max_vertices_num)
-    draw_graph(adjacency_matrix1)
-    adjacency_matrix1 = add_weights(adjacency_matrix1)
-    print(adjacency_matrix1)
+    draw_two_graphs(adjacency_matrix1, generate_random_graph(max_vertices_num))
+    adjacency_matrix_with_weights = add_int_weights(adjacency_matrix1)
+    print("graph1 weights: ", adjacency_matrix_with_weights)
