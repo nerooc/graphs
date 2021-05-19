807a70fa0310adb20144bc7c7abe00ebcb7f36e9
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-05-19 19:50:59 +0200

    task 2 update

--- Lab02/task2.py
+++ Lab02/task2.py
@@ -2,16 +2,13 @@ import random
 import time
 import sys
 from pandas import *
-from lab01 import read_graph_file_return_Adjacency_matrix
-from lab01 import generate_graph_b
-from lab01 import draw_graph
+from lab01 import read_graph_file_return_Adjacency_matrix, generate_graph_b, draw_graph
 from lab02 import *
-
 
 
 # main
 if __name__ == "__main__":
-    # # some tests
+    # # speed test
     # prop = [0.01, 0.2, 0.4, 0.6, 0.8, 0.99]
     # ver = [20, 100, 300, 500]
     # arrTime = [[0 for i in range(len(prop))] for row in range(len(ver))]
@@ -27,6 +24,16 @@ if __name__ == "__main__":
     # print(DataFrame(arrTime))
     # print()
 
+    # #  probability test
+    # count = 0
+    # for i in range(10000):
+    #     # adjacency_matrix1 = [[0, 0, 1, 1], [0, 0, 0, 1], [1, 0, 0, 0], [1, 1, 0, 0]] #50%
+    #     adjacency_matrix1 = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 0], [0, 0, 0, 0, 0]]  # 50%
+    #     # adjacency_matrix1 = [[0, 1, 0, 0, 0], [1, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 0, 1, 0, 1], [0, 0, 0, 1, 0]] #25%
+    #     adjacency_matrix2 = graph_randomization(adjacency_matrix1)
+    #     if adjacency_matrix1 == adjacency_matrix2:
+    #         count += 1
+    # print("In graph with 2 possible states probability to stay the same = ", count/100, "%")
 
     print("This is graph randomization. Path to graph data file should be typed in input args")
     file_name = 'InputFiles/input.txt'
@@ -35,6 +42,6 @@ if __name__ == "__main__":
     else:
         file_name = sys.argv[1]  # path to file with graph data
 
-    Adjacency_matrix = read_graph_file_return_Adjacency_matrix(file_name)
-    draw_two_graphs(Adjacency_matrix, graph_randomization(Adjacency_matrix),
+    adjacency_matrix = read_graph_file_return_Adjacency_matrix(file_name)
+    draw_two_graphs(adjacency_matrix, graph_randomization(adjacency_matrix, True),
                     'before randomization:', 'after randomization:')
