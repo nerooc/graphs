ebc7ebf1e1d364bcf1af91fb6e1e9545ad673668
Bartosz Rogowski <65545041+bartosz-rogowski@users.noreply.github.com>
2021-05-20 20:59:11 +0200

    Updated task2 - reading file containing adj matrix

--- Lab04/task2.py
+++ Lab04/task2.py
@@ -9,16 +9,35 @@ Script by Bartosz Rogowski
 
 import numpy as np
 from lab04 import kosaraju, drawDigraph
+import os
+import sys
+
+if len(sys.argv) > 2:
+	print('You have specified too many arguments')
+	sys.exit()
+
+if len(sys.argv) < 2:
+	print('You did not specify the location of input file')
+	sys.exit()
 
 if __name__ == '__main__':
-	try:
-		file_name = input("Please type file name:\n")
-		G = np.loadtxt(file_name, delimiter=" ")
-	except OSError:
-		print(f"{file_name} has not been found")
+	file_name=sys.argv[1]
+	if(os.path.isfile(file_name)):
+		if(os.path.getsize(file_name) == 0):
+			print(f"Sorry, file named {file_name} is empty.")
+			exit(-1)
+		try:
+			G = np.loadtxt(file_name, delimiter=" ")
+			comp = kosaraju(G)
+			drawDigraph(G, comp)
+		except ValueError:
+			print(f"{file_name} contains incorrect data (probaly not separated by spaces)")
+			exit(-1)
+		except Exception as e:
+			print(e)
+			exit(-1)
+	else:
+		print(f"Sorry, there is no file named {file_name}.")
 		exit(-1)
-	except ValueError:
-		print(f"{file_name} contains incorrect data (probaly not separated by spaces)")
-		exit(-1)
-	comp = kosaraju(G)
-	drawDigraph(G, comp)
+
+	
