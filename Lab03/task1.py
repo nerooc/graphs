dbbbe7870be6d455e534414e07a3703b63cc766e
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-05-21 20:44:22 +0200

    final update

--- Lab03/task1.py
+++ Lab03/task1.py
@@ -1,6 +1,8 @@
 import sys
-from lab02 import draw_two_graphs
-from lab03 import generate_random_graph, add_int_weights
+import numpy as np
+from lab01 import draw_graph
+from lab02 import draw_two_graphs, pretty_print
+from lab03 import generate_random_graph, add_int_weights, visualizeMST
 
 if __name__ == "__main__":
     print("This program generates random complement graph. In input args set max vertices number.")
@@ -9,16 +11,23 @@ if __name__ == "__main__":
         print("No input in program args, max_vertices_num = 9")
     else:
         max_vertices_num = int(sys.argv[1])
-    # max_vertices_num = 4
     adjacency_matrix1 = generate_random_graph(max_vertices_num)
-    draw_two_graphs(adjacency_matrix1, generate_random_graph(max_vertices_num))
     adjacency_matrix_with_weights = add_int_weights(adjacency_matrix1)
+    if len(adjacency_matrix1) == 1:
+        draw_graph(adjacency_matrix1)
+    else:
+        visualizeMST(np.array(adjacency_matrix_with_weights), [0])
     with open("graph.txt", "w") as f:
-        for row in adjacency_matrix1:
-            for col in row:
-                f.write(str(col)+" ")
-            f.write("\n")
+        if len(adjacency_matrix1) == 1:
+            f.write(str(adjacency_matrix1[0]))
+        else:
+            for row in adjacency_matrix1:
+                for col in row:
+                    f.write(str(col)+" ")
+                f.write("\n")
         f.close()
     print("graph1 weights: ")
-    for row in adjacency_matrix_with_weights:
-        print(row)
+    if len(adjacency_matrix1) == 1:
+        print(adjacency_matrix1[0])
+    else:
+        pretty_print(adjacency_matrix_with_weights)
