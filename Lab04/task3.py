c8273c61ea46fa54881c4ec95ad9140d368a9d07
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-06-02 16:51:10 +0200

    ready to send v2

--- Lab04/task3.py
+++ Lab04/task3.py
@@ -1,6 +1,4 @@
-from lab04 import generate_real_random_complement_digraph, add_weights_digraph, drawDigraph, kosaraju, bellman_ford, read_matrix_from_file
-from lab02 import pretty_print
-import numpy as np
+from lab04 import generate_real_random_complement_digraph, add_weights_digraph, drawDigraph, kosaraju, bellman_ford, read_matrix_from_file, conv_str_to_0_1, pretty_print_w_inf
 import sys
 
 if __name__ == '__main__':
@@ -10,9 +8,6 @@ if __name__ == '__main__':
     digraph = []
     if len(sys.argv) == 2:
         digraph = read_matrix_from_file(sys.argv[1])
-        for i in range(len(digraph)):
-            for j in range(len(digraph[0])):
-                digraph[i][j] = int(digraph[i][j])
         source_vertex = int(input("Choose source vertex to Bellman-Ford (first vertex is 0): "))
     else:
         if len(sys.argv) == 3:
@@ -21,15 +16,11 @@ if __name__ == '__main__':
         else:
             print("No correct input in program args, max_vertices_num = 7, source_vertex = 0")
         digraph = generate_real_random_complement_digraph(max_vertices_num)
-        add_weights_digraph(digraph, -5, 40)
-
-    array = np.array(digraph)
-    for i in range(len(array)):
-        for j in range(len(array[0])):
-            if array[i][j] != 0:
-                array[i][j] = 1
-
-    pretty_print(digraph)
+        digraph = add_weights_digraph(digraph, -5, 40)
+    print()
+    print("Graph with weights: ")
+    pretty_print_w_inf(digraph)
     print()
     bellman_ford(digraph, source_vertex, True)
-    drawDigraph(array, kosaraju(array))
+    drawDigraph(conv_str_to_0_1(digraph), kosaraju(conv_str_to_0_1(digraph)))
+
