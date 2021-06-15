ba7bd57d00c9182a57b8a23bbeec5844719dc11a
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-06-15 21:44:15 +0200

    add parameter to generate_random_graph (compatible to last update)

--- Lab03/task1.py
+++ Lab03/task1.py
@@ -2,16 +2,21 @@ import sys
 import numpy as np
 from lab01 import draw_graph
 from lab02 import draw_two_graphs, pretty_print
-from lab03 import generate_random_graph, add_int_weights, visualizeMST
+from lab03 import generate_random_graph, add_int_weights, visualizeMST, read_graph
 
 if __name__ == "__main__":
-    print("This program generates random complement graph. In input args set max vertices number.")
+    print("This program generates random coherent graph.")
+    print("In input args set max vertices number and probability p: (G(n,p)).")
     max_vertices_num = 9
+    probability = -1.0  # means random in generate_random_graph function
     if len(sys.argv) == 1:
-        print("No input in program args, max_vertices_num = 9")
+        print("No input in program args, max_vertices_num = 9, probability = random")
+    elif len(sys.argv) == 2:
+        max_vertices_num = int(sys.argv[1])
     else:
         max_vertices_num = int(sys.argv[1])
-    adjacency_matrix1 = generate_random_graph(max_vertices_num)
+        probability = float(sys.argv[2])
+    adjacency_matrix1 = generate_random_graph(max_vertices_num, probability)
     adjacency_matrix_with_weights = add_int_weights(adjacency_matrix1)
     if len(adjacency_matrix1) == 1:
         draw_graph(adjacency_matrix1)
@@ -31,3 +36,4 @@ if __name__ == "__main__":
         print(adjacency_matrix1[0])
     else:
         pretty_print(adjacency_matrix_with_weights)
+
